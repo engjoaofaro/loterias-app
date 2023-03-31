7c1ba4b248548924118ebea26aa95408c7ba3ed9
João Faro <projetos@joaofaro.eng.br>
2023-03-31 18:15:28 -0300

    commented price variable

--- src/service/impl/LotoFacil.java
+++ src/service/impl/LotoFacil.java
@@ -2,13 +2,11 @@ package service.impl;
 
 import service.Game;
 
-import java.math.BigDecimal;
-
 public class LotoFacil extends Game {
 
     private static final Integer MAX_LOTOFACIL = 25;
     private static final Integer MIN_NUMBER_GAME = 15;
-    private static final BigDecimal PRICE = new BigDecimal("2.50");
+    //private static final BigDecimal PRICE = new BigDecimal("2.50");
     @Override
     protected String generator(int quantity, int numbersOfGame) {
         if (!canPlayGame(numbersOfGame)) {
